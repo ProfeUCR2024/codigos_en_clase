b7cd9d93a69ee349fc82628ee8c0181a744ec54d
ProfeUCR2024 <fernando.corrales.76@gmail.com>
2024-04-16 19:50:51 -0600

    Cambio realizado a script.js LFCM

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let varaibleTres = '25'; /*los string siempre van en comillas simples o dobles*/
 let variableCuatro = 35;
 
 
-suma = variableUno+variableDos;
+suma = variableUno*variableDos;
 resta = variableUno-variableDos;
 
 // console.log('El resultado de la operacion es: ',resta)
